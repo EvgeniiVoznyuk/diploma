37a77a1a0820c50a239b771f25a1a6ceea8cded5
Evgenii <voznukzhenia@gmail.com>
2021-05-16 18:54:01 +0300

    client part

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,15 @@
 const ApiError = require('../error/ApiError')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const { User } = require('../models/models')
+
+const generetaJWT = (id, email, role) => {
+  return jwt.sign(
+    {id, email, role},
+    process.env.SECRET_KEY,
+    {expiresIn: '24h'}
+  )
+}
 
 class UserController {
   async registration(req, res, next) {
@@ -9,18 +18,43 @@ class UserController {
       return next(ApiError.badRequest('invalid email or password'))
     }
 
+    const candidate = await User.findOne({where: {email}})
+    if (candidate) {
+      return next(ApiError.badRequest('a user with this email was found'))
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5)
+    const user = await User.create({email, role, password: hashPassword})
+    const token = generetaJWT(user.id, user.email, user.role)
+
+    return res.json({token})
+
   }
 
-  async login(req, res) {
+  async login(req, res, next) {
+    const { email, password } = req.body
+    const user = await User.findOne({where: {email}})
 
+    if (!user) {
+      return next(ApiError.internal('User was not found'))
+    }
+
+    let comparePassword = bcrypt.compareSync(password, user.password)
+    console.log(comparePassword)
+
+    if (comparePassword) {
+      const token = generetaJWT(user.id, user.email, user.role)
+
+      return res.json({token})
+    } else {
+      return res.json({message: 'invalid data'})
+    }
   }
 
   async check(req, res, next) {
-    const {id} = req.query
-    if (!id) {
-      return next(ApiError.badRequest('id not found'))
-    }
-    res.json(id)
+    const token = generetaJWT(req.user.id, req.user.email, req.user.role)
+
+    return res.json({token})
   }
 }
 
